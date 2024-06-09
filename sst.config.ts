2b5a7b6005599e189e9a356947f21d3b9cde51b5
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-09 22:24:12 +0000

    updated GHA to sudo power

--- sst.config.ts
+++ sst.config.ts
@@ -24,54 +24,10 @@ function IAM({ app, stack }: StackContext) {
       description: "Role assumed for deploying from GitHub CI using AWS CDK",
       roleName: "GitHub", // Change this to match the role name in the GitHub workflow file
       maxSessionDuration: Duration.hours(1),
-      inlinePolicies: {
-        // You could attach AdministratorAccess here or constrain it even more, but this uses more granular permissions used by SST
-        SSTDeploymentPolicy: new iam.PolicyDocument({
-          assignSids: true,
-          statements: [
-            new iam.PolicyStatement({
-              effect: iam.Effect.ALLOW,
-              actions: [
-                "cloudformation:DeleteStack",
-                "cloudformation:DescribeStackEvents",
-                "cloudformation:DescribeStackResources",
-                "cloudformation:DescribeStacks",
-                "cloudformation:GetTemplate",
-                "cloudformation:ListImports",
-                "ecr:CreateRepository",
-                "iam:PassRole",
-                "iot:Connect",
-                "iot:DescribeEndpoint",
-                "iot:Publish",
-                "iot:Receive",
-                "iot:Subscribe",
-                "lambda:GetFunction",
-                "lambda:GetFunctionConfiguration",
-                "lambda:UpdateFunctionConfiguration",
-                "s3:ListBucket",
-                "s3:PutObjectAcl",
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:DeleteObject",
-                "s3:ListObjectsV2",
-                "s3:CreateBucket",
-                "s3:PutBucketPolicy",
-                "ssm:DeleteParameter",
-                "ssm:GetParameter",
-                "ssm:GetParameters",
-                "ssm:GetParametersByPath",
-                "ssm:PutParameter",
-                "sts:AssumeRole",
-              ],
-              resources: ["*"],
-            }),
-          ],
-        }),
-      },
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess")],
     });
   }
 }
-
 
 export default {
   config(_input) {
@@ -91,4 +47,3 @@ export default {
     app.stack(IAM);
   },
 } satisfies SSTConfig;
-
