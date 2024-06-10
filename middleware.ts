2879b603f0936519f7b9f9345fe7557bb4ff8e90
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-10 23:50:39 +0000

    writing a cookie to track the scores

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+export function middleware(request: NextRequest) {
+  if (request.cookies.get("id")) {
+    return NextResponse.next();
+  }
+  const response = NextResponse.next();
+  response.cookies.set("id", uuidv4());
+  return response;
+}
+
+// export const config = {
+//     matcher: '/',
+// }
