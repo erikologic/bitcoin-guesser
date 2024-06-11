072183aa80e34cfa68acaa993dadd42ef9481fc8
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-11 21:07:46 +0000

    refactor into a class unfortunately

--- app/gameEngine.spec.ts
+++ app/gameEngine.spec.ts
@@ -1,27 +1,10 @@
-// The player can choose to enter a guess of either “up” or “down“
 // After a guess is entered the player cannot make new guesses until the existing guess is resolved
 // The guess is resolved when the price changes and at least 60 seconds have passed since the guess was made
 // If the guess is correct (up = price went higher, down = price went lower), the user gets 1 point added to their score. If the guess is incorrect, the user loses 1 point.
 // Players can only make one guess at a time
 // New players start with a score of 0
 
-function getGame() {
-  return {
-    getScore: () => 0,
-    getPrice: () => 1000,
-  };
-}
-
-test("player can see their current score", () => {
-  const game = getGame();
-  expect(game.getScore()).toEqual(0);
-});
-
-test("player can see the latest available BTC price in USD", () => {
-  const game = getGame();
-  expect(game.getPrice()).toEqual(1000);
-});
-
+// The player can choose to enter a guess of either “up” or “down“
 type Guess = "up" | "down";
 
 class Game {
@@ -31,10 +14,28 @@ class Game {
     this.guess = guess;
   }
 
+  getScore() {
+    return 0;
+  }
+
+  getPrice() {
+    return 1000;
+  }
+
   canGuess() {
     return !this.guess;
   }
 }
+
+test("player can see their current score", () => {
+  const game = new Game();
+  expect(game.getScore()).toEqual(0);
+});
+
+test("player can see the latest available BTC price in USD", () => {
+  const game = new Game();
+  expect(game.getPrice()).toEqual(1000);
+});
 
 test("after a guess is entered the player cannot make new guesses until the existing guess is resolved", () => {
   const game = new Game();
