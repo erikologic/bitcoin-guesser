f4b8b22fde01fdd66d92fd05880b5a99d6779eab
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-11 21:03:30 +0000

    see current stuff

--- app/gameEngine.spec.ts
+++ app/gameEngine.spec.ts
@@ -1,95 +1,24 @@
+// The player can choose to enter a guess of either “up” or “down“
+// After a guess is entered the player cannot make new guesses until the existing guess is resolved
 // The guess is resolved when the price changes and at least 60 seconds have passed since the guess was made
 // If the guess is correct (up = price went higher, down = price went lower), the user gets 1 point added to their score. If the guess is incorrect, the user loses 1 point.
+// Players can only make one guess at a time
+// New players start with a score of 0
 
-const RESOLVE_THRESHOLD_MS = 60 * 1000;
-interface GuessEvent {
-  type: "guess";
-  timestamp: Date;
-  direction: "up" | "down";
-}
 
-function guess(timestamp: Date, direction: "up" | "down"): GuessEvent {
+function getGame() {
   return {
-    type: "guess",
-    timestamp,
-    direction,
+    getScore: () => 0,
+    getPrice: () => 1000,
   };
 }
 
-interface PriceEvent {
-  type: "price";
-  timestamp: Date;
-  price: number;
-}
-
-function price(timestamp: Date, price: number): PriceEvent {
-  return {
-    type: "price",
-    timestamp,
-    price,
-  };
-}
-
-type GameEvent = GuessEvent | PriceEvent;
-
-function canPlayerMakeANewGuess(
-  events: GameEvent[],
-  timestamp?: Date
-): boolean {
-  timestamp ||= new Date();
-
-  const guessEvents: GuessEvent[] = events
-    .filter((e) => e.type === "guess")
-    .sort(
-      (a, b) => b.timestamp.getTime() - a.timestamp.getTime()
-    ) as GuessEvent[];
-
-  if (guessEvents.length === 0) return true;
-
-  const lastGuessHasNotResolved =
-    guessEvents[0].timestamp.getTime() + RESOLVE_THRESHOLD_MS >
-    timestamp.getTime();
-  if (lastGuessHasNotResolved) return false;
-
-  return true;
-}
-
-function getPlayerScore(events: GameEvent[]): number {
-  let score = 0;
-  if (events.length === 0) return score;
-
-  throw new Error("Not implemented");
-}
-
-// @ts-ignore
-const dt = (...o) => new Date(...o);
-
-test("After a guess is entered the player cannot make new guesses until the existing guess is resolved", () => {
-  const events = [guess(dt("2024-01-01 00:00:00"), "up")];
-  expect(canPlayerMakeANewGuess(events, dt("2024-01-01 00:00:59"))).toBeFalsy();
-  expect(
-    canPlayerMakeANewGuess(events, dt("2024-01-01 00:01:00"))
-  ).toBeTruthy();
+test("player can see their current score", () => {
+  const game = getGame();
+  expect(game.getScore()).toEqual(0);
 });
 
-test("Initially a player can guess", () => {
-  const events: GameEvent[] = [];
-  expect(
-    canPlayerMakeANewGuess(events, dt("2024-01-01 00:01:00"))
-  ).toBeTruthy();
-});
-
-test("The guess is resolved when the price changes and at least 60 seconds have passed since the guess was made", () => {
-  expect(getPlayerScore([])).toBe(0);
-
-  const events = [
-    price(dt("2024-01-01 00:00:00"), 10000),
-    guess(dt("2024-01-01 00:00:00"), "up"),
-    price(dt("2024-01-01 00:01:00"), 10001),
-  ];
-
-  expect(getPlayerScore(events)).toBe(1);
-});
-
-
-// Can't I just use last guess and last price???
+test("player can see the latest available BTC price in USD", () => {
+  const game = getGame();
+  expect(game.getPrice()).toEqual(1000);
+})
