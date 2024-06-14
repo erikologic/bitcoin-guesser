d48587709551f38b8c27bff61cfb5a8f58178407
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-14 21:51:11 +0000

    Enable player to enter guess of "up" or "down"

--- app/page.spec.tsx
+++ app/page.spec.tsx
@@ -1,5 +1,3 @@
-// The player can at all times see their current score and the latest available BTC price in USD
-// The player can choose to enter a guess of either “up” or “down“
 // After a guess is entered the player cannot make new guesses until the existing guess is resolved
 // The guess is resolved when the price changes and at least 60 seconds have passed since the guess was made
 // If the guess is correct (up = price went higher, down = price went lower), the user gets 1 point added to their score. If the guess is incorrect, the user loses 1 point.
@@ -30,4 +28,10 @@ test('The player can at all times see their current score', async () => {
 test('The player can at all times the latest available BTC price in USD', async () => {
   render(await Home())
   expect(screen.getByRole('status', {name: "Price"})).toHaveTextContent('1000')
+})
+
+test('The player can choose to enter a guess of either “up” or “down“', async () => {
+  render(await Home())
+  expect(screen.getByRole('button', {name: "up"})).toBeEnabled()
+  expect(screen.getByRole('button', {name: "down"})).toBeEnabled()
 })
