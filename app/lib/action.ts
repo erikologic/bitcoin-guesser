b7623f85b4827170d4eb25518a711dc706be48ba
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-15 23:55:12 +0000

    I did it!

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,7 +4,6 @@ import { fetchBitcoinPrice } from "./btcService";
 import { putGuess } from "./game";
 
 export const guess = async (direction: "Up" | "Down") => {
-  // TODO rename to guess
   const btcPrice = await fetchBitcoinPrice();
   await putGuess(direction, btcPrice.data.rateUsd, btcPrice.timestamp);
 };
