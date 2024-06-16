7517b683ab716ce4b187e6f11aa7798f84d23140
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-16 10:07:10 +0000

    header & BTC price

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,40 @@ import { Button } from "./components/Button";
 import { Refresher } from "./components/Refresher";
 import { getState } from "./lib/game";
 
+import { Andika, Roboto_Mono } from "next/font/google";
+
+const andika = Andika({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '700',
+})
+
+const robotoMono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+})
+
+const formatRate = (rate: string) => parseFloat(rate).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+
 export default async function Home() {
   const {userState, btcRate} = await getState()
-  
   return (
     <>
       <Refresher />
-      <header>
-      <h1>Bitcoin Guesser</h1>
+      <header className="flex justify-center">
+        <h1 className={`${andika.className} uppercase text-4xl text-yellow-300 py-6`}>Bitcoin Guesser</h1>
       </header>
 
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-start">
+        <div role="status" aria-label="Price" className="flex flex-col items-center pb-6">
+            <div><span className="text-2xl">$ {formatRate(btcRate.rate)}</span></div>
+            <div className={`${robotoMono.className} uppercase text-xs text-blue-300 pt-2`}>
+              {new Date(btcRate.timestamp).toISOString()}
+            </div>
+        </div>
         <div role="status" aria-label="Score">
           Current Score: {userState.score}
-        </div>
-        <div role="status" aria-label="Price">
-            BTC Price: ${btcRate.rate} <br />
-            BTC time: {new Date(btcRate.timestamp).toLocaleString()}
         </div>
         {userState.guess.type === "unresolved" && <>
           <section role="status" aria-label="Guess">
