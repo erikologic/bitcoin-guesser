1eb9755d0a12fbfccea17f069d86653b0c7ac082
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-16 11:03:46 +0000

    Guess done

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,16 +2,41 @@
 
 import { useRouter } from "next/navigation";
 import { guess } from "../lib/game";
+import clsx from 'clsx';
 
 interface Props {
     direction: 'Up' | 'Down';
 }
 
+const ArrowDown = () => (
+    <svg  className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 30 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25" />
+    </svg>
+)
+
+const ArrowUp = () => (
+    <svg  className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 30 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+    </svg>
+)
+
 export const Button = ({direction}: Props) => {
     const router = useRouter();
 
-    return <button aria-label={direction} onClick={async () => {
-        await guess(direction);
-        router.refresh();
-    }}>{direction}</button>;
+    const isUp = direction === 'Up';
+
+    return <button 
+        className={clsx(" text-white font-bold py-2 px-4 border rounded inline-flex items-center",{
+            'bg-red-600 hover:bg-red-700 border-red-700': !isUp,
+            'bg-green-600 hover:bg-green-700 border-green-700': isUp
+        })}
+        aria-label={direction} 
+        onClick={async () => {
+                await guess(direction);
+                router.refresh();
+            }}
+        >
+              {isUp ? <ArrowUp /> : <ArrowDown />}
+              <span>{direction}</span>
+        </button>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { Button } from "./components/Button";
 import { Refresher } from "./components/Refresher";
 import { getState } from "./lib/game";
@@ -23,20 +25,35 @@ export default async function Home() {
   return (
     <>
       <Refresher />
-      <header className="flex justify-center">
-        <h1 className={`${andika.className} uppercase text-4xl text-yellow-300 py-6`}>Bitcoin Guesser</h1>
+      <header className="flex flex-col justify-center items-center pt-6 ">
+        <h1 className={`${andika.className} uppercase text-4xl text-orange-300 pb-5 `}>Bitcoin Guesser</h1>
+        <hr className="w-2/5 border-gray-700 mt-1 mb-5" />
       </header>
-
+      
       <main className="flex min-h-screen flex-col items-center justify-start">
-        <div role="status" aria-label="Price" className="flex flex-col items-center pb-6">
-            <div><span className="text-2xl">$ {formatRate(btcRate.rate)}</span></div>
-            <div className={`${robotoMono.className} uppercase text-xs text-blue-300 pt-2`}>
+        <div role="status" aria-label="Price" className="flex flex-col items-center pb-2">
+            <div className="flex">
+              <Image
+                  src="/bitcoin.svg"
+                  alt="Bitcoin Logo"
+                  width={30}
+                  height={24}
+                  priority
+                />
+              <div className="ml-2 text-2xl">$ {formatRate(btcRate.rate)}</div>
+            </div>
+            <div className={`${robotoMono.className} uppercase text-xs text-orange-200 pt-2`}>
               {new Date(btcRate.timestamp).toISOString()}
             </div>
         </div>
+
+        <hr className="w-2/5 border-gray-700 my-4" />
+
         <div role="status" aria-label="Score">
-          Current Score: {userState.score}
+          Your Score: {userState.score}
         </div>
+        <hr className="w-2/5 border-gray-700 my-4" />
+
         {userState.guess.type === "unresolved" && <>
           <section role="status" aria-label="Guess">
             Your guess: {userState.guess.direction} <br />
@@ -48,15 +65,20 @@ export default async function Home() {
           <section role="status" aria-label="Guess">
             Good Job! Your guess was correct! <br />
           </section>
-        </>}
+        </>}  
         {(userState.guess.type === "resolved" && !userState.guess.wasCorrect) && <>
           <section role="status" aria-label="Guess">
             Oh snaps! Your guess was uncorrect! <br />
           </section>
         </>}
         {userState.guess.type !== "unresolved" ? <>
-          <Button direction="Up" />
-          <Button direction="Down" />
+        <div className="flex flex-col items-center">
+          <h2>Guess where is going to move next?</h2>
+          <div className="flex justify-between space-x-4 mt-2">
+            <Button direction="Down" />
+            <Button direction="Up" />
+          </div>
+        </div>
         </> : <div>Waiting for the guess to resolve...</div>}
         
       </main>
